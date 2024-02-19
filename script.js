90d82f0ad5646f1efbca435b15aaa39898e95df5
aeroxego <aeroxe.gossiaux@gmail.com>
2024-02-19 19:20:13 +0100

    tentrative de resolution du scroll horiz sur mobile

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 icons.addEventListener("click", () => {
     nav.classList.toggle("active")
 })
+// Désactiver le défilement horizontal
+function disableHorizontalScroll() {
+    // Écoutez l'événement de défilement sur la fenêtre
+    window.addEventListener('scroll', function() {
+        // Si le défilement horizontal est détecté
+        if (window.scrollX !== 0) {
+            // Réinitialisez la position de défilement horizontal à 0
+            window.scrollTo(0, window.scrollY);
+        }
+    });
+}
+
+// Appel de la fonction pour désactiver le défilement horizontal
+disableHorizontalScroll();
